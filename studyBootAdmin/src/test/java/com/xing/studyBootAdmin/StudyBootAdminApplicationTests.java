e26663ec64e945b0941a45df89d3771850b6a550
xinghua <854592400@qq.com>
2021-11-01 18:54:18 +0800

    SpringBoot Admin Server 完毕

---
+++ studyBootAdmin/src/test/java/com/xing/studyBootAdmin/StudyBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xing.studyBootAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
